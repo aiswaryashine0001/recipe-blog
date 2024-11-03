f0df482d7d47b0eeb942ee64098f05adedac1562
aiswaryashine0001 <aiswaryashine11b@gmail.com>
2024-11-03 16:15:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ CORS(app)  # Allow CORS for all routes
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="your-password",
+    password="root",
     database="receipeblog"
 )
 
